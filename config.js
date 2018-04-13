eeaaf5fcc92fce2e8f4f97fbe453fb5ec2b15237
Geoffrey ROYER <geoffrey.royer@gmail.com>
2018-04-13 08:59:42 +0200

    Reformat code

--- config.js
+++ config.js
@@ -5,29 +5,28 @@ let extend = require('extend');
 let environment = args.env || "test";
 
 let common_conf = {
-    name: "meuporg PoC of MMO Game Server",
-    version: "0.0.1",
-    environment: environment,
-    max_players: 100,
-    data_paths: {
-        items: __dirname + "/game_data/items/",
-        maps: __dirname + "/game_data/maps/",
-    },
-    starting_zone: "rm_map_home",
+  name: "meuporg PoC of MMO Game Server",
+  version: "0.0.1",
+  environment: environment,
+  max_players: 100,
+  data_paths: {
+    items: "/game_data/items/",
+    maps: "/game_data/maps/",
+  },
+  starting_zone: "rm_map_home",
 };
 
 let conf = {
-    production: {
-        ip: args.ip || "0.0.0.0",
-        port: args.port || 8081,
-        database: "mongodb://127.0.0.1/meuporg_prod"
-    },
-
-    test: {
-        ip: args.ip || "0.0.0.0",
-        port: args.port || 8082,
-        database: "mongodb://127.0.0.1/meuporg_test"
-    }
+  production: {
+    ip: args.ip || "0.0.0.0",
+    port: args.port || 8081,
+    database: "mongodb://127.0.0.1/meuporg_prod"
+  },
+  test: {
+    ip: args.ip || "0.0.0.0",
+    port: args.port || 8082,
+    database: "mongodb://127.0.0.1/meuporg_test"
+  }
 }
 
 extend(false, conf.production, common_conf);
--- server.js
+++ server.js
@@ -3,26 +3,25 @@ let fs = require('fs');
 let net = require('net');
 
 let loadResources = (dir, callback=undefined) => {
-    let files = fs.readdirSync(__dirname + '/' + dir);
-    if (callback === undefined) {
-        files.forEach((file) => {
-            console.log('Loading ' + dir + ': ' + file);
-            require(dir + '/' + file);
-        });
-    } else {
-        files.forEach(callback);
-    }
+  let files = fs.readdirSync(__dirname + '/' + dir);
+  if (callback === undefined) {
+    files.forEach((file) => {
+      console.log('Loading ' + dir + ': ' + file);
+      require(dir + '/' + file);
+    });
+  } else {
+    files.forEach(callback);
+  }
 }
 
 let maps = {};
 
-try {
-    loadResources("initializers");
-    loadResources("models");
-    loadResources(config.data_paths.maps, (mapFile) => {
-        console.log("Loading map: " + config.data_paths.maps + mapFile);
-        maps[map.room] = map;
-    });
-} catch(error) {
-    console.error("Error while loading resources: " + error);
-}
+loadResources("initializers");
+loadResources("models");
+loadResources(dir=config.data_paths.maps, (mapFile) => {
+  console.log("Loading map: " + dir + mapFile);
+  var map = require(__dirname + dir + mapFile);
+  maps[map.room] = map;
+});
+
+console.log(maps);

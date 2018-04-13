fd14a67d1bb62d00c26ba076eb7f285c2c85b27d
Geoffrey ROYER <geoffrey.royer@gmail.com>
2018-04-13 09:18:45 +0200

    Refactor server + start listening

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-require(__dirname + "/config.js");
+require(`${__dirname}/config.js`);
 let fs = require('fs');
 let net = require('net');
 
 let loadResources = (dir, callback=undefined) => {
-  let files = fs.readdirSync(__dirname + '/' + dir);
+  let files = fs.readdirSync(`${__dirname}/${dir}`);
   if (callback === undefined) {
     files.forEach((file) => {
-      console.log('Loading ' + dir + ': ' + file);
-      require(dir + '/' + file);
+      console.log(`Loading ${dir}${mapFile}`);
+      require(`${dir}/${file}`);
     });
   } else {
     files.forEach(callback);
@@ -19,9 +19,12 @@ let maps = {};
 loadResources("initializers");
 loadResources("models");
 loadResources(dir=config.data_paths.maps, (mapFile) => {
-  console.log("Loading map: " + dir + mapFile);
+  console.log(`Loading map: ${dir}${mapFile}`);
   var map = require(__dirname + dir + mapFile);
   maps[map.room] = map;
 });
 
-console.log(maps);
+net.createServer((socket) => {
+}).listen(config.port);
+
+console.log(`Initialize completed. Server running on port ${config.port}`);

a00c6afe7b67e23242fe0b31e501bd1d01c6bfce
Geoffrey ROYER <geoffrey.royer@gmail.com>
2018-04-13 08:38:10 +0200

    Edit server.js

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
 require(__dirname + "/config.js");
+let fs = require('fs');
+let net = require('net');
 
-console.log(config.database);
+let loadResources = (dir, callback=undefined) => {
+    let files = fs.readdirSync(__dirname + '/' + dir);
+    if (callback === undefined) {
+        files.forEach((file) => {
+            console.log('Loading ' + dir + ': ' + file);
+            require(dir + '/' + file);
+        });
+    } else {
+        files.forEach(callback);
+    }
+}
+
+let maps = {};
+
+try {
+    loadResources("initializers");
+    loadResources("models");
+    loadResources(config.data_paths.maps, (mapFile) => {
+        console.log("Loading map: " + config.data_paths.maps + mapFile);
+        maps[map.room] = map;
+    });
+} catch(error) {
+    console.error("Error while loading resources: " + error);
+}
